c657e09cdd8cfcce177ed2c9d9e68ae8dc1ebfc9
David Wales <daviewales@disroot.org>
2025-03-07 14:38:31 +1100

    ascceg: Include broad and narrow groups

--- src/abs/ascceg.py
+++ src/abs/ascceg.py
@@ -19,15 +19,34 @@ def df():
     # Load both sheets at once to avoid downloading the file twice
     dfs = pl.read_excel(
         ascceg_file,
-        sheet_name=["Table 1.3", "Table 2"],
+        sheet_name=["Table 1.1", "Table 1.2", "Table 1.3", "Table 2"],
         has_header=False,
+    )
+
+    broad_groups = (
+        dfs["Table 1.1"]
+        .with_row_index()
+        .filter(col("index") >= 5)
+        .select(col("column_1").alias("broad_code"), col("column_2").alias("broad_group"))
+        .filter(col("broad_code").str.contains(digits))
+    )
+
+    narrow_groups = (
+        dfs["Table 1.2"]
+        .with_row_index()
+        .filter(col("index") >= 7)
+        .select(col("column_2").alias("narrow_code"), col("column_3").alias("narrow_group"))
+        .filter(col("narrow_code").str.contains(digits))
     )
 
     main_groups = (
         dfs["Table 1.3"]
         .with_row_index()
         .filter(col("index") >= 9)
-        .select(col("column_3").alias("code"), col("column_4").alias("group"))
+        .select(
+            col("column_3").alias("code"),
+            col("column_4").alias("cultural_and_ethnic_group"),
+        )
         .filter(col("code").str.contains(digits))
     )
 
@@ -35,11 +54,20 @@ def df():
         dfs["Table 2"]
         .with_row_index()
         .filter(col("index") >= 4)
-        .select(col("column_1").alias("code"), col("column_2").alias("group"))
+        .select(
+            col("column_1").alias("code"),
+            col("column_2").alias("cultural_and_ethnic_group"),
+        )
         .filter(col("code").str.contains(digits))
         .with_columns(col("code").str.pad_start(4, "0"))
     )
 
-    all_groups = pl.concat([main_groups, supplementary_groups]).sort("code")
+    all_groups = (
+        pl.concat([main_groups, supplementary_groups])
+        .join(broad_groups, left_on=col("code").str.slice(0, 1), right_on="broad_code", how='full')
+        .join(narrow_groups, left_on=col("code").str.slice(0, 2), right_on="narrow_code", how='full')
+        .select("code", "broad_group", "narrow_group", "cultural_and_ethnic_group")
+        .sort("code")
+    )
 
     return all_groups
